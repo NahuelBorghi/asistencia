e324f6688510a1bd93c2db33f4d59f39f6e3f3c2
NahuelBorghi <nahuelborghi@gmail.com>
2023-04-30 21:46:08 -0300

    ya funciona todo lo del UML. ahora hay que ampliar

--- Components/attendanceManager.py
+++ Components/attendanceManager.py
@@ -1,25 +1,31 @@
 from course import Course
 from student import Student
 import pandas as pd
+import os
 
 class attendanceMgr:
     # Constructor
     def __init__(self) -> None:
-        pass
+        file_path = './data/attendance.csv'
+        if os.path.isfile(file_path):
+            self.attendanceData = pd.read_csv(file_path)
+        else:
+            self.attendanceData = pd.DataFrame(columns=["IdEstudiante", "Nombre/s", "Apellido/s", "IdMateria", "NombreMateria", "MarcaTemporal", "Presente"])
+            self.attendanceData.to_csv(file_path, index=False)
     # Metodos
-    def takeAtt(student:Student, course:Course, datetime:str, state:bool) -> None:
-        '''data = {'Id de Estudiante':student.getID(),'Nombre/s':student.getName(),'Apellido/s':student.getSurName(),'Id de Materia':1,'Nombre de Materia':course.getName(),'Marca temporal':datetime,'Presente':state}
-        columns=["IdEstudiante", "Nombre/s", "Apellido/s", "IdMateria", "NombreMateria", "MarcaTemporal", "Presente"]
-        df = pd.DataFrame(data, columns=columns, index=columns)
-        df.to_csv('../data/attendance.csv', index=False)'''
-        #todavia no entiendo como funcionan los argumentos de to_csv
+    def takeAtt(self, student:Student, course:Course, datetime:str, state) -> None:
+        newAttendanceData = pd.DataFrame({
+            "IdEstudiante":[student.getID()],
+            "Nombre/s":[student.getName()],
+            "Apellido/s":[student.getSurName()],
+            "IdMateria":[course.getID()],
+            "NombreMateria":[course.getName()],
+            "MarcaTemporal":[datetime],
+            "Presente":[state]
+        })
+        self.attendanceData = pd.concat([self.attendanceData, newAttendanceData], ignore_index=True)
+        self.attendanceData.to_csv('./data/attendance.csv', index=False)
         pass
-    def showAtt() -> None:
+    def showAtt(self) -> None:
+        print(self.attendanceData)
         pass
-
-
-
-att = attendanceMgr
-alum = Student(42235717,"nahuel","borghi")
-curso = Course(1,"bases de datos")
-att.takeAtt(alum,curso,"28-04-2023",True)
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import sys
+sys.path.append('./Components')
+
 from Components.attendanceManager import attendanceMgr
+from Components.course import Course
+from Components.student import Student
+
+
+att = attendanceMgr()
+alum = Student(42235717,"nahuel","borghi")
+curso = Course(1,"bases de datos")
+att.takeAtt(alum,curso,"28-04-2023",1)
+att.showAtt()
